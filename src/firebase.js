fa065ea53434750e83fa0c8c3559fa2f3c093435
Aditya14320 <dwivediraj304@gmail.com>
2026-05-12 12:44:14 +0530

    add vercel routing

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3RMOf0gjNPZ1PPhK2Sj9IT6OZ3SsSgzE",
-  authDomain: "camchat-ee539.firebaseapp.com",
-  projectId: "camchat-ee539",
-  storageBucket: "camchat-ee539.firebasestorage.app",
-  messagingSenderId: "52527857412",
-  appId: "1:52527857412:web:ea44b9e160138984d95a51",
-  measurementId: "G-EDN85V1WM8"
+  apiKey: "AIzaSyAg0v4rVwQ9-PNu7cVl8NGubelIvQG1FLA",
+  authDomain: "amdd-76ff2.firebaseapp.com",
+  projectId: "amdd-76ff2",
+  storageBucket: "amdd-76ff2.firebasestorage.app",
+  messagingSenderId: "428992181441",
+  appId: "1:428992181441:web:cbb92248b9648fededd040",
+  measurementId: "G-4N7MPJR87R"
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = initializeFirestore(app, {
   localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() })
