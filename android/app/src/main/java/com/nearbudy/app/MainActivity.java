4113fd081f5bcce7d178531ee674a8c0b245bb3b
Aditya14320 <dwivediraj304@gmail.com>
2026-06-13 10:20:48 +0530

    Updated project

--- android/app/src/main/java/com/nearbudy/app/MainActivity.java
+++ android/app/src/main/java/com/nearbudy/app/MainActivity.java
@@ -13,7 +13,17 @@ public class MainActivity extends BridgeActivity implements ModifiedMainActivity
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN && requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+        // Handle Google Social Login result
+        // Some Android versions/libraries truncate request codes to 16 bits (0-65535)
+        int googleMin = GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN;
+        int googleMax = GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX;
+        int truncatedMin = googleMin & 0xFFFF;
+        int truncatedMax = googleMax & 0xFFFF;
+
+        boolean isGoogleRequest = (requestCode >= googleMin && requestCode < googleMax) ||
+                                 (requestCode >= truncatedMin && requestCode < truncatedMax);
+
+        if (isGoogleRequest) {
             PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
             if (pluginHandle != null) {
                 ((SocialLoginPlugin) pluginHandle.getInstance()).handleGoogleLoginIntent(requestCode, data);
